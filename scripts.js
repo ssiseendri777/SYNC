48469b0c3b0d71530c18811925ffeaccc2704fc1
data-analysis-project <ssiseendri777@gmail.com>
2026-09-02 12:38:27 +0530

    completed the home page

--- scripts.js
+++ scripts.js
@@ -1,15 +1,26 @@
 const toggleBtn = document.getElementById("darkToggle");
 const icon = toggleBtn.querySelector("i");
 
-toggleBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-
-  // Switch icon between moon and sun
-  if (document.body.classList.contains("dark")) {
+// Apply saved mode when page loads
+if (localStorage.getItem("darkMode") === "enabled") {
+    document.body.classList.add("dark-mode");
     icon.classList.remove("fa-moon");
     icon.classList.add("fa-sun");
-  } else {
-    icon.classList.remove("fa-sun");
-    icon.classList.add("fa-moon");
-  }
+}
+
+// Toggle dark mode
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+
+        localStorage.setItem("darkMode", "enabled");
+    } else {
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
+
+        localStorage.setItem("darkMode", "disabled");
+    }
 });
